d976b3c32738fdf1bbf69982ed540b246bcb4bba
wapppress <wapppress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 04:15:56 +0000

    Fixed Issue.
    git-svn-id: https://plugins.svn.wordpress.org/wapppress-builds-android-app-for-website/trunk@1121032 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/ajax_handler.php
+++ includes/ajax_handler.php
@@ -60,7 +60,7 @@ if( isset($_POST['type']) && $_POST['type'] =='api_create_form') {
 	function get_domain($url){
 	  $pieces = parse_url($url);
 	  $domain = isset($pieces['host']) ? $pieces['host'] : '';
-	  if(preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,6})$/i', $domain, $regs)) {
+	  if(preg_match('/(?P<domain>[a-z0-9][a-z0-9\-]{1,63}\.[a-z\.]{2,10})$/i', $domain, $regs)) {
 		return $regs['domain'];
 	  }
 	  return false;
